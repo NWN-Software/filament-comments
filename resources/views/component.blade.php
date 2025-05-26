8576d3050ba52e6c2666543e2150bdc56fba9a70
chandraauliatama <chandraaulia4@gmail.com>
2025-05-26 19:20:11 +0700

    Small Fix Typo

--- resources/views/component.blade.php
+++ resources/views/component.blade.php
@@ -1,6 +1,6 @@
 <livewire:comments 
     :record="$record ?? $this->record" 
     :resource="$resource" 
-    :sentMailWhenTagged="$sentMailWhenTagged ?? false"
+    :sendMailWhenTagged="$sendMailWhenTagged ?? false"
     :mailSubjectForTaggedUsers="$mailSubjectForTaggedUsers ?? ''"
 />
